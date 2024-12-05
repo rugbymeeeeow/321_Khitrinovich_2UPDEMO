21a2ac434d5ca2bbe79e328b9aa280ccec5c25c3
217097 <217097@KB37-120-C08>
2024-12-05 13:37:14 +0300

    Добавьте файлы проекта.

---
+++ 321_Khitrinovich_2UP/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _321_Khitrinovich_2UP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseWindow(object sender, System.ComponentModel.CancelEventArgs e) 
+        {
+            MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите выйти?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.No) { e.Cancel = true; }
+        }
+        private void MainFrame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            if (!(e.Content is Page page)) return;
+            this.Title = $"ProjectByKhitrinovich - {page.Title}";
+
+            if (page is AllPages.Menu)
+                BackButton.Visibility = Visibility.Hidden;
+            else
+                BackButton.Visibility = Visibility.Visible;
+        }
+
+        private void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoBack) NavigationService.GoBack();
+        }
+    }
+}
